6a22c11ecbe68bbde0c6e0e291a0be4d6b6783b4
IsaacIngram <isaac@isaacingram.com>
2024-02-16 19:54:23 -0500

    feat: implement update needs (put)
    Implement the PUT REST request to update needs given their ID

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/persistence/NeedDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/persistence/NeedDAO.java
@@ -1,4 +1,17 @@
 package com.ufund.api.ufundapi.model.persistence;
 
+import com.ufund.api.ufundapi.model.Need;
+
+import java.io.IOException;
+
 public interface NeedDAO {
+
+    /**
+     * Updates and saves a Need
+     * @param need Need to be updated and saved
+     * @return Updated Need if successful, null if it could not be found
+     * @throws IOException If underlying storage can not be accessed
+     */
+    Need updateNeed(Need need) throws IOException;
+
 }
