a39184873cefcb3423d2aa78a9bf91572b246023
IsaacIngram <me@isaacingram.dev>
2024-02-18 16:30:57 -0500

    test: add FileDAO test for getting multiple needs

--- ufund-api/src/test/java/com/ufund/api/ufundapi/model/persistence/NeedFileDAOTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/model/persistence/NeedFileDAOTest.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ufund.api.ufundapi.model.Need;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -37,6 +39,22 @@ public class NeedFileDAOTest {
         when(mockObjectMapper.readValue(new File("anything.txt"), Need[].class))
                 .thenReturn(testNeeds);
         needFileDao = new NeedFileDAO("anything.txt", mockObjectMapper);
+    }
+
+    /**
+     * Test getting multiple Needs
+     */
+    @Test
+    public void testGetNeeds() {
+        // Invoke
+        Need[] needs = needFileDao.getNeeds();
+
+        // Analyze
+        assertEquals(needs.length, testNeeds.length);
+        for(int i = 0; i < testNeeds.length; i++) {
+            assertEquals(needs[i], testNeeds[i]);
+        }
+
     }
 
 }
