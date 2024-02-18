c1e806e5a637767d6638ce2fd7cacf9f62660565
IsaacIngram <isaac@isaacingram.com>
2024-02-17 20:09:25 -0500

    test: Initial commit of Need test framework
    Initial commit of Need test framework. Contains a basic test that
    creates a new need with certain attributes and then checks those
    attributes to make sure they are equal

---
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/model/NeedTest.java
@@ -0,0 +1,42 @@
+package com.ufund.api.ufundapi.model;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test suite for the Need class.
+ */
+@Tag("Model-tier")
+public class NeedTest {
+
+    @Test
+    public void testGetAttributes() {
+
+        // Setup expected data
+        int expected_id = 3;
+        String expected_name = "Water";
+        float expected_price = 3.21f;
+        int expected_quantity = 5;
+
+        // Invoke creation and storage
+        Need need = new Need(
+                expected_id,
+                expected_name,
+                expected_price,
+                expected_quantity
+        );
+
+        // Analyze
+        assertEquals(expected_id, need.getId());
+        assertEquals(expected_name, need.getName());
+        assertEquals(expected_price, need.getPrice());
+        assertEquals(expected_quantity, need.getQuantity());
+    }
+
+    public void testSetId() {
+
+    }
+
+}
