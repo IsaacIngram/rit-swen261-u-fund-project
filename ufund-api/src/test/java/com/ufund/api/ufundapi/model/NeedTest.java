f8d5568051221acdbbe9c06e21d6abce2e0d9bdd
IsaacIngram <me@isaacingram.dev>
2024-02-18 13:14:25 -0500

    test: Add tests for the Need class
    Add tests for the functions in the Need class

--- ufund-api/src/test/java/com/ufund/api/ufundapi/model/NeedTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/model/NeedTest.java
@@ -38,8 +38,104 @@ public class NeedTest {
         assertEquals(expected_quantity, need.getQuantity());
     }
 
-    public void testSetId() {
+    /**
+     * Test setting the name of a Need
+     */
+    @Test
+    public void testSetName() {
+        // Setup
+        int id = 3;
+        String old_name = "Water";
+        String new_name = "Ice";
+        float price = 3.21f;
+        int quantity = 5;
 
+        // Invoke
+        Need need = new Need(
+                id,
+                old_name,
+                price,
+                quantity
+        );
+        need.setName("Ice");
+
+        // Analyze
+        assertEquals(new_name, need.getName());
+    }
+
+    /**
+     * Test setting the price of a Need
+     */
+    @Test
+    public void testSetPrice() {
+        // Setup
+        int id = 3;
+        String name = "Water";
+        float old_price = 3.21f;
+        float new_price = 6.78f;
+        int quantity = 5;
+        Need need = new Need(
+                id,
+                name,
+                old_price,
+                quantity
+        );
+
+        // Invoke
+        need.setPrice(new_price);
+
+        // Analyze
+        assertEquals(new_price, need.getPrice());
+    }
+
+    /**
+     * Test setting the quantity of a Need
+     */
+    @Test
+    public void testSetQuantity() {
+        // Setup
+        int id = 3;
+        String name = "Water";
+        float price = 3.21f;
+        int old_quantity = 5;
+        int new_quantity = 8;
+        Need need = new Need(
+                id,
+                name,
+                price,
+                old_quantity
+        );
+
+        // Invoke
+        need.setQuantity(new_quantity);
+
+        // Analyze
+        assertEquals(new_quantity, need.getQuantity());
+    }
+
+    /**
+     * Test getting the string of a Need
+     */
+    @Test
+    public void testToString() {
+        // Setup
+        int id = 3;
+        String name = "Water";
+        float price = 3.21f;
+        int quantity = 5;
+        String expected_string = String.format(Need.STRING_FORMAT, id, name, price, quantity);
+        Need need = new Need(
+                id,
+                name,
+                price,
+                quantity
+        );
+
+        // Invoke
+        String actual_string = need.toString();
+
+        // Analyze
+        assertEquals(expected_string, actual_string);
     }
 
 }
