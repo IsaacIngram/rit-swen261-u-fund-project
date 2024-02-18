602f7126c3b082a24474309311152637d6f0e069
IsaacIngram <me@isaacingram.dev>
2024-02-18 17:08:01 -0500

    test: add test case for controller getneeds()
    Add a test case for the getneeds() method for the NeedController

--- ufund-api/src/test/java/com/ufund/api/ufundapi/viewmodel/NeedControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/viewmodel/NeedControllerTest.java
@@ -1,10 +1,20 @@
 package com.ufund.api.ufundapi.viewmodel;
 
+import com.ufund.api.ufundapi.model.Need;
 import com.ufund.api.ufundapi.model.persistence.NeedDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test the Need Controller class
@@ -23,6 +33,41 @@ public class NeedControllerTest {
     public void setupNeedController() {
         mockNeedDao = mock(NeedDAO.class);
         needController = new NeedController(mockNeedDao);
+    }
+
+    /**
+     * Test get needs
+     * @throws IOException if the DAO was unable to read the data
+     */
+    @Test
+    public void testGetNeeds() throws IOException {
+        // Setup
+        Need[] needs = new Need[3];
+        needs[0] = new Need(72, "Water", 1.00f, 72);
+        needs[1] = new Need(73, "Food", 12.34f, 3);
+        needs[2] = new Need(74, "Shelter", 1234.56f, 1);
+        // Tell the mockDAO to return the needs we created above
+        when(mockNeedDao.getNeeds()).thenReturn(needs);
+
+        // Invoke
+        ResponseEntity<Need[]> response = needController.getNeeds();
+
+        // Analyze
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Need[] response_body = response.getBody();
+        if(response_body == null) {
+            fail("Received null response body");
+        } else {
+            // Iterate through all responses
+            for (int i = 0; i < needs.length; i++) {
+                // Check that this response is in bounds of expected
+                if (i < response.getBody().length) {
+                    assertEquals(needs[i], response.getBody()[i]);
+                } else {
+                    fail("Response content is larger than expected output");
+                }
+            }
+        }
     }
 
 }
