7fcc78ba331402e0d4d821daf34361e55b79c8b8
unknown <clm6507>
2024-02-19 01:00:40 -0500

    feat: Stubbed out deleteNeed() in NeedDAO.java
    Added javadoc as well as basic function stub to the NeedDAO interface

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/persistence/NeedDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/persistence/NeedDAO.java
@@ -1,4 +1,20 @@
 package com.ufund.api.ufundapi.model.persistence;
 
+import java.io.IOException;
+
+import com.ufund.api.ufundapi.model.Need;
+
 public interface NeedDAO {
+    /**
+     * Deletes a {@linkplain Need need} with the given id
+     *
+     * @param id The id of the {@link Need need}
+     *
+     * @return true if the {@link Need need} was deleted
+     * <br>
+     * false if need with the given id does not exist
+     *
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    boolean deleteNeed(int id) throws IOException;
 }
