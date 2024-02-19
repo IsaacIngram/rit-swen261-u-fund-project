621c60d76149c89b04ee1151009e0f1907086f5c
unknown <clm6507>
2024-02-19 16:26:04 -0500

    feat: Added the createNeed() function and fully fleshed it out

--- ufund-api/src/main/java/com/ufund/api/ufundapi/viewmodel/NeedController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/viewmodel/NeedController.java
@@ -1,9 +1,17 @@
 package com.ufund.api.ufundapi.viewmodel;
 
+import com.ufund.api.ufundapi.model.Need;
 import com.ufund.api.ufundapi.model.persistence.NeedDAO;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
@@ -20,5 +28,35 @@ public class NeedController {
      */
     public NeedController(NeedDAO needDao) {
         this.needDao = needDao;
+    }
+
+
+    /**
+     * Creates a {@linkplain Need need} with the provided need object
+     *
+     * @param need - The {@link Need need} to create
+     *
+     * @return ResponseEntity with created {@link Need need} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link Need need} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("")
+    public ResponseEntity<Need> createNeed(@RequestBody Need need) {
+        LOG.info("POST /needs " + need);
+
+        try {
+            Need result = needDao.createNeed(need);
+            // I think this is supposed to be a null check? Docstring for createNeed
+            // says it returns "false" if creating a need wasn't successful, but that
+            // obviously isn't true because it returns a Need object...
+            if(result == null) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+                return new ResponseEntity<Need>(result, HttpStatus.OK);
+            }
+        } catch(IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
