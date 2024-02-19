7a3412ae0529581c0094a0864816f03d64ed2937
unknown <clm6507>
2024-02-19 16:11:58 -0500

    test: Added complete tests for the deleteNeed() in NeedControllerTest
    Tests every possible response from deleteNeed()

--- ufund-api/src/test/java/com/ufund/api/ufundapi/viewmodel/NeedControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/viewmodel/NeedControllerTest.java
@@ -1,10 +1,19 @@
 package com.ufund.api.ufundapi.viewmodel;
 
+import com.ufund.api.ufundapi.model.Need;
 import com.ufund.api.ufundapi.model.persistence.NeedDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 
 /**
  * Test the Need Controller class
@@ -23,6 +32,51 @@ public class NeedControllerTest {
     public void setupNeedController() {
         mockNeedDao = mock(NeedDAO.class);
         needController = new NeedController(mockNeedDao);
+    }
+    
+    
+    /**
+    * 
+    * @throws IOException
+    */
+    @Test
+    public void testDeleteNeedOk() throws IOException{
+        Need okNeed = new Need(1, "Food", 13.0f, 10);
+
+        //Creates the response that will be handled through the controler that matches the need in the controler.
+        //Doesn't look at the fileDAO that matches the ID, instead matches the id of the need that was created in the test file.
+        ResponseEntity<Need> response = needController.deleteNeed(1);
+        //checks if the response got the need that was grabbed from the controler need created
+        assertEquals(okNeed, response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    /**
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testGetNeedNotFound() throws IOException{
+        Need notFoundNeed = new Need(2, "Stuff", 1.5f, 100);
+
+        ResponseEntity<Need> respone = needController.deleteNeed(1);
+        assertNotEquals(notFoundNeed, null);
+        assertEquals(HttpStatus.NOT_FOUND, respone.getStatusCode());
+
+    }
+    /**
+    * 
+    * @throws IOException
+    */
+    @Test
+    public void testGetNeedServerError() throws IOException{
+        Need serverError = new Need(3, "Mihou", 2.78f, 13);
+        
+        when(mockNeedDao.deleteNeed(3)).thenThrow(new IOException());
+
+        ResponseEntity<Need> response = needController.deleteNeed(4);
+        assertEquals(serverError, response);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 
 }
