a96d9c0de598bfa80b82aa6083b1fa88922c0332
unknown <clm6507>
2024-02-19 19:45:35 -0500

    test: Got rid of unnecessary code from every function

--- ufund-api/src/test/java/com/ufund/api/ufundapi/viewmodel/NeedControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/viewmodel/NeedControllerTest.java
@@ -41,7 +41,6 @@ public class NeedControllerTest {
     */
     @Test
     public void testDeleteNeedOk() throws IOException{
-        Need okNeed = new Need(1, "Food", 13.0f, 10);
 
         when(mockNeedDao.deleteNeed(1)).thenReturn(true);
         //Creates the response that will be handled through the controler that matches the need in the controler.
@@ -58,7 +57,6 @@ public class NeedControllerTest {
      */
     @Test
     public void testDeleteNeedNotFound() throws IOException{
-        Need notFoundNeed = new Need(2, "Stuff", 1.5f, 100);
 
         when(mockNeedDao.deleteNeed(1)).thenReturn(false);
         ResponseEntity<Need> response = needController.deleteNeed(1);
@@ -72,7 +70,6 @@ public class NeedControllerTest {
     */
     @Test
     public void testDeleteNeedServerError() throws IOException{
-        Need serverError = new Need(3, "New_Need", 2.78f, 13);
         
         when(mockNeedDao.deleteNeed(4)).thenThrow(new IOException());
 
