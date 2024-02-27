24747fee7ac5bac0fbb4beb7c24db7c645a24af8
IsaacIngram <me@isaacingram.dev>
2024-02-27 17:24:25 -0500

    feat: Add placeholder getNeeds() functionality
    Add placeholder getNeeds() functionality that returns a pre-populated
    array of needs. This way the cupboard page can display some test data

--- ufund-ui/src/app/cupboard/cupboard.component.ts
+++ ufund-ui/src/app/cupboard/cupboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AccessControlService } from "../access-control.service";
+import {Need} from "../Need";
 
 @Component({
   selector: 'app-cupboard',
@@ -8,6 +9,31 @@ import { AccessControlService } from "../access-control.service";
 })
 export class CupboardComponent {
 
+
+  needs: Need[] = []
+
   constructor(protected accessControl: AccessControlService) { }
+
+  /**
+   * Built into Angular. Called when Angular is done initializing this
+   * component
+   */
+  ngOnInit(): void {
+    this.getNeeds();
+  }
+
+  /**
+   * Get all needs in the cupboard. Makes REST request to get all Needs from
+   * the server.
+   */
+  getNeeds(): void {
+    //TODO link to needs service
+    // For now, this just
+    this.needs = [
+      {id: 1, name: "water", type: "BEVERAGE", quantity: 3, price: 2.98},
+      {id: 2, name: "hamburger", type: "FOOD", quantity: 9, price: 12.34},
+      {id: 3, name: "$1000 fund", type: "MONETARY", quantity: 1000, price: 1.00}
+    ]
+  }
 
 }
