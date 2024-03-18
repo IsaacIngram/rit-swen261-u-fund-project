a1cddafc8019e15d34bb453323df6c368b735fc3
Johanna Wichmann <jsw7482@rit.edu>
2024-03-18 19:39:48 -0400

    Linked basket page to main page and added on buttons to basket page

--- ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from '../basket/basket.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'basket', component: BasketComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
