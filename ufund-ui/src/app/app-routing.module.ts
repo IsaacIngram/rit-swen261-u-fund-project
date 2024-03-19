85fa301196ad821818cbf8b91d59bf4f61325763
IsaacIngram <me@isaacingram.dev>
2024-03-19 14:28:11 -0400

    bugfix: resolve basketcomponent import
    Resolve basketcomponent import that caused the angular project to not
    build.

--- ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BasketComponent } from '../basket/basket.component';
+import { BasketComponent} from "./basket/basket.component";
 
 
 const routes: Routes = [
