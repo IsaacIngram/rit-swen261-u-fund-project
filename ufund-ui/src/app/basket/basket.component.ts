f5a8ca16ac3f02f47d75d099617d6ec76717262a
Johanna Wichmann <jsw7482@rit.edu>
2024-03-20 13:16:14 -0400

    Added docstrings to each function

--- ufund-ui/src/app/basket/basket.component.ts
+++ ufund-ui/src/app/basket/basket.component.ts
@@ -9,27 +9,41 @@ import { Component, OnInit } from '@angular/core';
 export class BasketComponent {
 
   static needs: Need[] = [];
-
+  /**
+   * Adds an item to the basket
+   * @param need the item being added to the basket
+   */
   static addToBasket(need: Need): void {
     BasketComponent.needs.push(need);
   }
-
+  /**
+   * Creates an empty basket
+   */
   clearBasket() {
     BasketComponent.needs = [];
   }
-
+  /**
+   * Get all needs that are in the basket
+   */
   getBasket(): Need[] {
     return BasketComponent.needs;
   }
-
+  /**
+   * Adjusts the quantity of each need in the basket
+   * @param need the need that the quanity is being adjusted of
+   * @param number either 1 or -1 depending on if a need is being increased or decreased
+   */
   adjustQuantity(need: Need, delta: number): void {
     need.quantity+=delta;
-
+    // stops need from going below at least 1 in the basket
     if(need.quantity < 1) {
       need.quantity = 1;
     }
   }
-
+  /**
+   * Removes a need from the basket
+   *@param need the need that is being removed 
+   */
   removeFromBasket(need: Need): void {
     BasketComponent.needs = BasketComponent.needs.filter(n => n !== need);
   }
