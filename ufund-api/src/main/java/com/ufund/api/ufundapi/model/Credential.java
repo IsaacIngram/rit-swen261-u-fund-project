17d0d57f677b63435560a9067a8668fe4ecd13b4
IsaacIngram <me@isaacingram.dev>
2024-04-02 16:40:36 -0400

    feat: add DAO interface for authentication
    Add all functions that the FileDAO for authentication will need

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Credential.java
@@ -0,0 +1,57 @@
+package com.ufund.api.ufundapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Credential {
+
+    static final String STRING_FORMAT = "Credential [username=%s, password=%s]";
+
+    @JsonProperty("username") private String username;
+    @JsonProperty("password") private String password;
+
+    /**
+     * Create a credential with the given properties
+     * @param username The username
+     * @param password The password hash as a string
+     */
+    public Credential(
+            @JsonProperty("username") String username,
+            @JsonProperty("password") String password
+    ) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Get the username of this credential
+     * @return String username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Check whether this credentials password hash equals a string
+     * @param comparison String to compare
+     * @return True if they match, False otherwise
+     */
+    public boolean comparePassword(String comparison) {
+        return comparison.equals(password);
+    }
+
+    /**
+     * Check if this credential equals another (by username only)
+     * @param o the object to compare
+     * @return True if the other object is a Credential with the same username,
+     * false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Credential) {
+            Credential object = (Credential)o;
+            return this.username.equals(object.getUsername());
+        }
+        return false;
+    }
+
+}
