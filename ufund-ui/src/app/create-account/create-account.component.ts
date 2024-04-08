638eb34d2ebe9fa4e2f580f73d4b16d42aeaa569
unknown <clm6507>
2024-04-08 09:16:13 -0400

    feat: Added createaccount button to the html and its basis in create-account.component.ts

--- ufund-ui/src/app/create-account/create-account.component.ts
+++ ufund-ui/src/app/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AccessControlService } from '../access-control.service';
 
 @Component({
   selector: 'app-create-account',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './create-account.component.css'
 })
 export class CreateAccountComponent {
+  private authService = inject(AccessControlService)
+
   charErrorVisible: boolean = false
+  nameTaken: boolean = false
+  passwordDontMatch: boolean = false
+
+  createaccount(login: HTMLInputElement, password: HTMLInputElement, checkpassword: HTMLInputElement){
+    if(password.value != checkpassword.value){
+      this.passwordDontMatch = true
+      return
+    }
+    
+  }
 }
